92b6fe4fe8c333e8b1702ca63d5b9225db4c174e
abdouDev22 <abdillahiabdourahman65@gmail.com>
2025-01-05 18:58:50 +0300

    amelioration cote resposive et quelque correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import './App.scss';
 
+// Composant pour gérer la redirection en fonction de la largeur de l'écran
+function ScreenWidthRedirect() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 900) {
+        navigate("/about");
+      }
+    };
+
+    // Vérifie la largeur de l'écran au chargement initial
+    handleResize();
+
+    // Écoute les changements de taille de la fenêtre
+    window.addEventListener("resize", handleResize);
+
+    // Nettoie l'écouteur d'événements lors du démontage du composant
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [navigate]);
+
+  return null; // Ce composant ne rend rien visuellement
+}
+
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        
+        {/* Intégration du composant de redirection */}
+        <ScreenWidthRedirect />
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-
         </Routes>
+        
         <Footer />
       </div>
     </Router>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,22 +12,26 @@ import Vector from '../../assets/header-img/Vector.svg';
 const Hero = () => {
   return (
     <section id='About' className="hero">
+      
       <div className='content'>
+       
         <div className="text-content">
           <img className='img-frame' src={Frame14} alt="Decorative frame" />
           <h1>I develop <img className='img' src={Computer} alt="Icon of a computer" /> <span >high-quality</span> <span className='color highlight'>software</span></h1>
           <button>See Projects <span className='tire'></span></button>
         </div>
+       
         <div className="illustration">
-          <span className='absolute A'><img src={A} alt="Decorative element A in hero section" /></span>
+          {/* <span className='absolute A'><img src={A} alt="Decorative element A in hero section" /></span>
           <span className='absolute B'><img src={B} alt="Decorative element B in hero section" /></span>
           <span className='absolute text'><img className='img-face' src={face} alt="Abstract face illustration" />Abdourahman Abdillahi</span>
           <span className='absolute fleche' aria-hidden="true"></span>
-          <img className='photo' src={Rface} alt="Photo of Abdourahman Abdillahi" />
+          <img className='photo' src={Rface} alt="Photo of Abdourahman Abdillahi" />*/}
         </div>
 
       </div>
       <img className='vector' src={Vector} alt="Decorative vector graphic" />
+      
     </section>
   );
 };
