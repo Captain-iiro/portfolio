070ed8e3e091d198f2d85b34a6d64bed03992456
abdouDev22 <abdillahiabdourahman65@gmail.com>
2025-01-06 12:28:26 +0300

    amelioration cote resposive et quelque correction

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
                 <h2>Start building your project today.</h2>
                 <p>Transform your ideas into a unique digital experience.</p>
                 <p>Got questions? Send us a message!</p>
-                <a   href="mailto:abdillahiabdourahman65@gmail.com"><button className='button'> Send us message <span className='tire'></span></button></a>
+                <a   href="mailto:abdillahiabdourahman65@gmail.com?subject=Portfolio abdourahmanabdillahi.com&body=Votre message ici"><button className='button'> Send us message <span className='tire'></span></button></a>
             </div>
         </section>
     );
