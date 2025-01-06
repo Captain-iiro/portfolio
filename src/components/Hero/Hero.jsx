d7fc20f16ef48618d951d7b839a77bab8a9bba95
abdouDev22 <abdillahiabdourahman65@gmail.com>
2025-01-06 11:22:45 +0300

    amelioration cote resposive et quelque correction

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,9 @@ import Computer from '../../assets/hero-img/computer.svg';
 import Rface from '../../assets/hero-img/Rface.webp';
 import face from '../../assets/hero-img/face.svg';
 import A from '../../assets/hero-img/3.svg';
+import A_mini from '../../assets/hero-img/3-mini.svg';
 import B from '../../assets/hero-img/4.svg';
+import B_min from '../../assets/hero-img/4-mini.svg';
 import Frame14 from '../../assets/hero-img/Frame14.svg';
 import Vector from '../../assets/header-img/Vector.svg';
 
@@ -22,8 +24,8 @@ const Hero = () => {
         </div>
        
         <div className="illustration">
-          <span className='absolute A'><img src={A} alt="Decorative element A in hero section" /></span>
-          <span className='absolute B'><img src={B} alt="Decorative element B in hero section" /></span>
+          <span className='absolute A'><picture><source media="(max-width: 900px)" srcSet={A_mini} /><img src={A} alt="Decorative element A in hero section" /></picture></span>
+          <span className='absolute B'><picture><source media="(max-width: 900px)" srcSet={B_min} /><img src={B} alt="Decorative element B in hero section" /></picture></span>
           <span className='absolute text'><img className='img-face' src={face} alt="Abstract face illustration" />Abdourahman Abdillahi</span>
           <span className='absolute fleche' aria-hidden="true"></span>
           <img className='photo' src={Rface} alt="Photo of Abdourahman Abdillahi" />
