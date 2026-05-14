3dacc37987c4367f3445c705c8f42128086b8ffc
Captain-iiro <abdillahiabdourahman65@gmail.com>
2026-05-14 08:43:58 +0300

    ajout responsive a la section projet

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -29,11 +29,22 @@ const Projects = () => {
             </div>
           </div>
           <div className="project">
-            {/* <div className="projet-c project-image"></div>
-            <div className="projet-c project-name">Project Name</div>
-            <div className="projet-c project-description">
-              Project Description
-            </div>*/}
+            <div className="projet-c">
+              <div className="project-image"></div>
+              <div className="projet-description">
+                <div className="project-titre">
+                  <div className="project-name">Project Name</div>
+                  <div className="project-sousTitre">
+                    UI design - User research - webflow develop
+                  </div>
+                </div>
+                <div className="projet-button">
+                  <Link to="/" className="project-link">
+                    <span className="text"></span>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
